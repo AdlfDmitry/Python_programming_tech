98a05558a6b36bca4ff72b706ca4a6904c060ac4
AdlfDmitry <144434078+AdlfDmitry@users.noreply.github.com>
2025-02-11 17:33:31 +0300

    Delete lab_03 directory

--- lab_03/items/students.py
+++
@@ -1,8 +0,0 @@
-class Student:
-    def __init__(self,name,phone,email,gender):
-        self.name = name
-        self.phone = phone
-        self.email = email
-        self.gender = gender
-    def __str__(self):
-        return f"Name: '{self.name}', phone: '{self.phone}', email: '{self.email}', gender: '{self.gender}' "
